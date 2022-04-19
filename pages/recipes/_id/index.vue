ec277f7a1d0a52e26390afd43d84f9fc9e5734c4
erminpajic <59607618+erminpajic@users.noreply.github.com>
2022-04-19 18:02:43 +0200

    install nuxt-app

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<section class="single-recipe">
+		<h1>{{ recipe.title }}</h1>
+		<div>
+			<img class="recipe-img" :src="recipe.thumbnail" :alt="recipe.title">
+		</div>
+		<p>{{ recipe.descriptionText }}</p>
+	</section>
+</template>
+
+<script>
+export default {
+	asyncData(context){
+		return new Promise ((resolve, reject) => {
+			setTimeout(() => {
+				resolve({
+					recipe:[
+						{
+							id: "1",
+							title: "Delicious Pizza",
+							previewText: "Awesome Pizza",
+							descriptionText: "proba lorem ipsum 123",
+							thumbnail: "https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900",
+						},
+						{
+							id: "2",
+							title: "Delicious Pizza 2",
+							previewText: "Awesome Pizza 2",
+							descriptionText: "proba lorem ipsum 123",
+							thumbnail: "https://static.toiimg.com/thumb/56933159.cms?imgsize=686279&width=800&height=800",
+						}
+					].find(el => el.id === context.params.id)
+				})
+			}, 1500);
+		})
+	}
+}
+</script>
+
+<style scoped>
+	.single-recipe{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		padding: 30px;
+		flex-flow: column;
+	}
+	.recipe-img{
+		width: 100%;
+	}
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,54 @@
+<template>
+	<section class="recipes">
+		<Recipe
+			v-for="recipe in recipes"
+			:key ="recipe.id"
+			:thumbnail="recipe.thumbnail"
+			:title="recipe.title"
+			:id="recipe.id"
+			:previewText="recipe.previewText"
+		/>
+
+	</section>
+</template>
+
+<script>
+	import Recipe from '@/components/Recipe'
+
+	export default{
+		components:{
+			Recipe
+		},
+		asyncData(){
+			return new Promise ((resolve, reject) => {
+				setTimeout(() => {
+					resolve({
+						recipes:[
+							{
+								id: "1",
+								title: "Delicious Pizza",
+								previewText: "Awesome Pizza",
+								thumbnail: "https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900",
+							},
+							{
+								id: "2",
+								title: "Delicious Pizza 2",
+								previewText: "Awesome Pizza 2",
+								thumbnail: "https://static.toiimg.com/thumb/56933159.cms?imgsize=686279&width=800&height=800",
+							}
+						]
+					})
+				}, 1500);
+			})
+		}
+	}
+</script>
+
+<style scoped>
+	.recipes{
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
